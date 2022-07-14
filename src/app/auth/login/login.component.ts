8d16264d47b136c6dfdf1d9758b9b2b8b9cc4c53
Daniel Aranda <daniel.aranda@accenture.com>
2022-07-14 12:36:57 -0500

    Create login form component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+    public formCustom: FormGroup = new FormGroup({});
 
-  constructor() { }
+    constructor(private _fb: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.createForm();
+    }
 
+    /**
+     * Method to create the login form
+     */
+    createForm() {
+        this.formCustom = this._fb.group({
+            emailAddress: ['', [Validators.required, Validators.email]],
+            password: ['', Validators.required],
+        });
+    }
+
+    /**
+     * Method to submit the login form
+     * @returns
+     */
+    loginUser() {
+        if (this.formCustom.invalid) return;
+        console.log(this.formCustom.value);
+    }
 }
