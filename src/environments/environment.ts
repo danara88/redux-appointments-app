f2f1f78d6831a32464f1d7c010a04a9e16c305da
Daniel Aranda <daniel.aranda@accenture.com>
2022-07-14 12:47:13 -0500

    Add firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyAgYBjRNeUuZFEkkTZxk21UIZkqjlcWWdM',
+        authDomain: 'redux-appointments-project.firebaseapp.com',
+        projectId: 'redux-appointments-project',
+        storageBucket: 'redux-appointments-project.appspot.com',
+        messagingSenderId: '39700790642',
+        appId: '1:39700790642:web:7c81ade2f6bfc41c6e71eb',
+    },
 };
 
 /*
