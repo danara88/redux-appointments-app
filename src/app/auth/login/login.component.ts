a6e5fb5669ab17c5b01978e8b199fbd57217674c
Daniel Aranda <C-Daniel.Aranda@charter.com>
2022-07-15 15:44:51 -0500

    Implement login fireebase and logout firebase

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FirebaseAuthError } from 'src/app/models/firebase.auth.models';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
     selector: 'app-login',
@@ -9,7 +13,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
     public formCustom: FormGroup = new FormGroup({});
 
-    constructor(private _fb: FormBuilder) {}
+    constructor(
+        private _fb: FormBuilder,
+        private _authService: AuthService,
+        private _router: Router
+    ) {}
 
     ngOnInit(): void {
         this.createForm();
@@ -29,8 +37,20 @@ export class LoginComponent implements OnInit {
      * Method to submit the login form
      * @returns
      */
-    loginUser() {
+    async loginUser() {
         if (this.formCustom.invalid) return;
-        console.log(this.formCustom.value);
+        const { emailAddress, password } = this.formCustom.value;
+        try {
+            await this._authService.loginUserFirebase(emailAddress, password);
+            this.formCustom.reset();
+            this._router.navigateByUrl('/home');
+        } catch (error) {
+            let errorObj = <FirebaseAuthError>error;
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: errorObj.message,
+            });
+        }
     }
 }
