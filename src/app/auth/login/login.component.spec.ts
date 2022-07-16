4695109a172966c4de722e8a812f590ef5c81d18
Daniel Aranda <C-Daniel.Aranda@charter.com>
2022-07-16 18:18:21 -0500

    Login integration test

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AngularFireModule } from '@angular/fire/compat';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { environment } from 'src/environments/environment';
@@ -19,7 +19,7 @@ describe('LoginComponent', () => {
                 AngularFireModule.initializeApp(environment.firebase),
                 RouterTestingModule,
             ],
-            providers: [AuthService],
+            providers: [AuthService, FormBuilder],
         }).compileComponents();
 
         fixture = TestBed.createComponent(LoginComponent);
@@ -38,5 +38,22 @@ describe('LoginComponent', () => {
         // Assert
         expect(component.formCustom.contains('emailAddress'));
         expect(component.formCustom.contains('password'));
+    });
+
+    it('should send email and password to login user in firebase', () => {
+        // Asset
+        const email = 'test@test.com';
+        const password = '123456';
+        const authService = TestBed.get(AuthService);
+        const spy = spyOn(authService, 'loginUserFirebase');
+
+        // Act
+        component.createForm();
+        component.formCustom.controls['emailAddress'].setValue(email);
+        component.formCustom.controls['password'].setValue(password);
+        component.loginUser();
+
+        // Assert
+        expect(spy).toHaveBeenCalledWith(email, password);
     });
 });
