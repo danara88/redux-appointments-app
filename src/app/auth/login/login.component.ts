da5611f03ee0ec214015b5b6046c923a70d7f523
Daniel Aranda <C-Daniel.Aranda@charter.com>
2022-07-19 14:21:41 -0500

    Remove unnecesary actions and reducers, implement spinner on login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { FirebaseAuthError } from 'src/app/models/firebase.auth.models';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import Swal from 'sweetalert2';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
     selector: 'app-login',
@@ -16,7 +17,8 @@ export class LoginComponent implements OnInit {
     constructor(
         private _fb: FormBuilder,
         private _authService: AuthService,
-        private _router: Router
+        private _router: Router,
+        private _spinner: NgxSpinnerService
     ) {}
 
     ngOnInit(): void {
@@ -40,12 +42,15 @@ export class LoginComponent implements OnInit {
     async loginUser() {
         if (this.formCustom.invalid) return;
         const { emailAddress, password } = this.formCustom.value;
+        this._spinner.show();
         try {
             await this._authService.loginUserFirebase(emailAddress, password);
             this.formCustom.reset();
             this._router.navigateByUrl('/home');
+            this._spinner.hide();
         } catch (error) {
             let errorObj = <FirebaseAuthError>error;
+            this._spinner.hide();
             Swal.fire({
                 icon: 'error',
                 title: 'Oops...',
